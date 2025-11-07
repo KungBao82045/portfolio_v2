6e5db4006b5ae22d424747ff0a3d43278ab76649
KungBao <caojacky2003@gmail.com>
2025-11-07 23:18:56 +0100

    delete files outside 'api_fetcher' folder

--- create_api.py
+++
@@ -1,8 +0,0 @@
-import datetime
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World", "timestamp": datetime.datetime.now()}
--- get_api.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-x = requests.get("http://localhost:8091/")
-print(x.json())
